bc43dba75484ddc866ef2cf9a6e17baf7e9d7eb1
MehmetUySaL07 <mehmetuysal_07@outlook.com>
2023-12-24 00:43:55 +0300

    .

---
+++ App.js
@@ -0,0 +1,68 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import CourseInput from './components/CourseInput';
+
+export default function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [courses, setCourses] = useState([]);
+  const startModal = () => {
+    setModalIsVisible(true);
+  };
+  const endModal = () => {
+    setModalIsVisible(false);
+  };
+
+  const addCourse = (courseTitle) => {
+    // console.log(courseTitle);
+    setCourses((currentCourses) => [
+      ...currentCourses,
+      { text: courseTitle, id: Math.random().toString() },
+    ]);
+    endModal();
+  };
+
+  return (
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Button title="Kurs Ekle" color="#1FFFE9" onPress={startModal} />
+        <CourseInput
+          visible={modalIsVisible}
+          onAddCourse={addCourse}
+          onCancel={endModal}
+        />
+        <View>
+          <FlatList
+            data={courses}
+            renderItem={({ item }) => (
+              <View style={styles.courseItem}>
+                <Text style={styles.courseText}>{item.text}</Text>
+              </View>
+            )}
+          />
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  courseItem:{
+    backgroundColor:'gray',
+    margin:8,
+    borderRadius:5,
+
+  },
+  courseText:{
+    padding:8,
+    color:'white',
+
+  }
+});
